b34633410311b74ced859698b326190182ef1553
joelfranklin22 <joeljeyam22@gmail.com>
2023-08-29 22:25:24 +0530

    Area of triangle

---
+++ triangle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class triangle
+{	
+	public static void main(String args[])
+	{
+		Scanner d=new Scanner(System.in);
+		System.out.print("enter the length:");
+		int l=d.nextInt();
+		System.out.print("enter the breadth:");
+		int b=d.nextInt();
+		System.out.println("Area of triangle ="+(0.5*l*b));
+	}
+}
