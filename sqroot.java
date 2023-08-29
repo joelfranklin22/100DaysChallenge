2d92934fe1ea65c23c349f165eed1d2e00cc7fe0
joelfranklin22 <joeljeyam22@gmail.com>
2023-08-29 22:23:55 +0530

    Square Root of number

---
+++ sqroot.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class sqroot
+{
+	public static void main(String args[])
+	{
+		Scanner d=new Scanner(System.in);
+		System.out.println("enter a value:");
+		int a=d.nextInt();
+		System.out.println("Square root of a number"+Math.sqrt(a));
+	}
+}
