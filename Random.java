80e0605bbab34915a5108ba1cc0398c8824186e4
joelfranklin22 <joeljeyam22@gmail.com>
2023-09-02 22:56:59 +0530

    Generate Random Numbers

---
+++ Random.java
@@ -0,0 +1,10 @@
+public class Random
+{
+	public static void main(String args[])
+	{
+		for(int i=0;i<10;i++)
+	//Random Function is used 
+	//Normally it generates Value between 0 and 1 
+			System.out.println("Random Number "+(i+1) +":"+Math.random()); 
+	}
+}
