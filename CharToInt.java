5017e518c6b1dec6af47953864692788653b463b
joelfranklin22 <joeljeyam22@gmail.com>
2023-09-18 17:35:53 +0530

    Convert Char to Int

---
+++ CharToInt.java
@@ -0,0 +1,10 @@
+class CharToInt {
+  public static void main(String[] args) {
+    char a = '5';
+    char b = 'c';
+    int num1 = a;
+    int num2 = b;
+    System.out.println(num1);    
+    System.out.println(num2);    
+  }
+}
