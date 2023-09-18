f3b346ec8ad5af16ac4813ef628937fee8394850
joelfranklin22 <joeljeyam22@gmail.com>
2023-09-18 17:35:24 +0530

    Convert Int to Char

---
+++ IntToChar.java
@@ -0,0 +1,10 @@
+class  IntToChar{
+  public static void main(String[] args) {
+    int num1 = 80;
+    int num2 = 81;
+    char a = (char)num1;
+    char b = (char)num2;
+    System.out.println(a);    
+    System.out.println(b);    
+  }
+}
