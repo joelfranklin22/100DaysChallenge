6fc61a90bbc725cace6f82a47a8e96fe004fce07
joelfranklin22 <joeljeyam22@gmail.com>
2023-09-20 22:04:14 +0530

    Check String

---
+++ check.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class check
+{
+	public static void main(String args[])
+	{
+		int j;
+		String temp; 
+		Scanner d=new Scanner(System.in);
+		String a[]=new String[200];
+		int n=1;
+		for(int i=0;i<n;i++)
+		{
+				System.out.print("Enter the String :");			
+				a[i]=d.nextLine();		
+		}
+	}
+}
