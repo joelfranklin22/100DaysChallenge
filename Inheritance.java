a4ab8eae8c5958760b6685aed669df4ecdaf6a78
Joel Franklin J <123979098+joelfranklin22@users.noreply.github.com>
2023-10-17 20:59:16 +0530

    Inheritance in java

---
+++ Inheritance.java
@@ -0,0 +1,41 @@
+public class Employee {
+    private int salary;
+
+    public Employee(int salary) {
+        this.salary = salary;
+    }
+
+    public void work() {
+        System.out.println("working as an employee!");
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}	
+public class HRManager extends Employee {
+    public HRManager(int salary) {
+        super(salary);
+    }
+
+    public void work() {
+        System.out.println("\nManaging employees");
+    }
+
+    public void addEmployee() {
+        System.out.println("\nAdding new employee!");
+    }
+}
+public class Inheritance {
+    public static void main(String[] args) {
+        Employee emp = new Employee(40000);
+        HRManager mgr = new HRManager(70000);
+
+        emp.work();
+        System.out.println("Employee salary: " + emp.getSalary());
+
+        mgr.work();
+        System.out.println("Manager salary: " + mgr.getSalary());
+        mgr.addEmployee();
+    }
+}
