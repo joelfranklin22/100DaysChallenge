32c78faecd8fb17df269ea13a592108ee83b1447
Joel Franklin J <123979098+joelfranklin22@users.noreply.github.com>
2023-10-25 22:45:53 +0530

    Words Count in String Type

---
+++ WordsCount.java
@@ -0,0 +1,7 @@
+public class WordsCount {
+  public static void main(String[] args) {
+    String words = "One Two Three Four";
+    int countWords = words.split("\\s").length;
+    System.out.println(countWords);
+  }
+}
